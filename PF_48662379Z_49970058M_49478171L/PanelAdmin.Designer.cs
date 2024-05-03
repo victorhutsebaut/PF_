7df31df34f1f4b48e3d623e713395ded4c9f49b2
verde <eduardo.rodacarrasco@gmail.com>
2024-05-03 17:51:37 +0200

    terminado admin

--- PF_48662379Z_49970058M_49478171L/PanelAdmin.Designer.cs
+++ PF_48662379Z_49970058M_49478171L/PanelAdmin.Designer.cs
@@ -29,16 +29,15 @@
         private void InitializeComponent()
         {
             this.listViewusuarios = new System.Windows.Forms.ListView();
+            this.colum_nombre = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colum_contra = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.btn_eliminar = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.btn_añadir = new System.Windows.Forms.Button();
             this.txb_usuario = new System.Windows.Forms.TextBox();
             this.txb_contraseña = new System.Windows.Forms.TextBox();
-            this.colum_nombre = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.colum_contra = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // listViewusuarios
@@ -56,6 +55,16 @@
             this.listViewusuarios.View = System.Windows.Forms.View.Details;
             this.listViewusuarios.SelectedIndexChanged += new System.EventHandler(this.listViewusuarios_SelectedIndexChanged);
             // 
+            // colum_nombre
+            // 
+            this.colum_nombre.Text = "Usuario";
+            this.colum_nombre.Width = 366;
+            // 
+            // colum_contra
+            // 
+            this.colum_contra.Text = "Contraseña";
+            this.colum_contra.Width = 386;
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -70,25 +79,16 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Yu Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(47, 825);
+            this.label2.Location = new System.Drawing.Point(393, 683);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(159, 35);
             this.label2.TabIndex = 2;
             this.label2.Text = "Contraseña";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Yu Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(1219, 479);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(0, 35);
-            this.label3.TabIndex = 3;
-            // 
             // btn_eliminar
             // 
             this.btn_eliminar.Font = new System.Drawing.Font("Yu Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_eliminar.Location = new System.Drawing.Point(533, 708);
+            this.btn_eliminar.Location = new System.Drawing.Point(936, 152);
             this.btn_eliminar.Name = "btn_eliminar";
             this.btn_eliminar.Size = new System.Drawing.Size(244, 88);
             this.btn_eliminar.TabIndex = 4;
@@ -100,7 +100,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Yu Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(410, 825);
+            this.label4.Location = new System.Drawing.Point(863, 50);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(442, 70);
             this.label4.TabIndex = 5;
@@ -109,7 +109,7 @@
             // btn_añadir
             // 
             this.btn_añadir.Font = new System.Drawing.Font("Yu Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_añadir.Location = new System.Drawing.Point(47, 981);
+            this.btn_añadir.Location = new System.Drawing.Point(749, 708);
             this.btn_añadir.Name = "btn_añadir";
             this.btn_añadir.Size = new System.Drawing.Size(261, 88);
             this.btn_añadir.TabIndex = 6;
@@ -128,32 +128,21 @@
             // txb_contraseña
             // 
             this.txb_contraseña.Font = new System.Drawing.Font("Yu Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txb_contraseña.Location = new System.Drawing.Point(47, 894);
+            this.txb_contraseña.Location = new System.Drawing.Point(393, 752);
             this.txb_contraseña.Name = "txb_contraseña";
             this.txb_contraseña.Size = new System.Drawing.Size(261, 51);
             this.txb_contraseña.TabIndex = 8;
-            // 
-            // colum_nombre
-            // 
-            this.colum_nombre.Text = "Usuario";
-            this.colum_nombre.Width = 366;
-            // 
-            // colum_contra
-            // 
-            this.colum_contra.Text = "Contraseña";
-            this.colum_contra.Width = 386;
             // 
             // PanelAdmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1912, 1139);
+            this.ClientSize = new System.Drawing.Size(1421, 907);
             this.Controls.Add(this.txb_contraseña);
             this.Controls.Add(this.txb_usuario);
             this.Controls.Add(this.btn_añadir);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.btn_eliminar);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listViewusuarios);
@@ -169,7 +158,6 @@
         private System.Windows.Forms.ListView listViewusuarios;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btn_eliminar;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btn_añadir;
