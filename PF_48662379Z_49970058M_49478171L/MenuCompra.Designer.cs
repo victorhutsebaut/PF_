14680e9b1e615a548342381b8904d8206c8f086e
Alejandro Nicolás Hernández <alejandro26marzo@gmail.com>
2024-05-05 18:31:26 +0200

    jj

--- PF_48662379Z_49970058M_49478171L/MenuCompra.Designer.cs
+++ PF_48662379Z_49970058M_49478171L/MenuCompra.Designer.cs
@@ -424,7 +424,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(464, 204);
+            this.textBox2.Location = new System.Drawing.Point(464, 217);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 40);
             this.textBox2.TabIndex = 51;
